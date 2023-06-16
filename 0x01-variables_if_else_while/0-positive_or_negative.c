38aef56d8c32af25668b70ccd252009bf58cccc7
Mohamed marouane <mohmd8mar@gmail.com>
2023-06-16 16:42:24 -0700

    fisrt task #0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /* more headers goes there */
-
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 /* betty style doc for function main goes there */
 int main(void)
 {
@@ -13,7 +17,7 @@ int main(void)
 	/* your code goes there */
 	if (n > 0) 
 		printf("%d is positive\n", n);
-	else if(n < 0)
+	else if (n < 0)
 		printf("%d is negative\n", n);		
 	else 
 		printf("%d is zero\n", n);
