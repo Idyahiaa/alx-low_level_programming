29558e30785fc0efe29af268711ae5e3b1841bf3
Mohamed marouane <mohmd8mar@gmail.com>
2023-06-16 19:16:02 -0700

    task 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,9 +11,11 @@ int main(void)
 	int num = '0';
 	for( num = '0'; num <= '9'; num++ )
 	{
+
 		putchar(num);
-		putchar(',');
-		putchar(' ');
+		if (num != '9')
+			putchar(',');
+			putchar(' ');
 	}
 putchar('\n');
 	return (0);
