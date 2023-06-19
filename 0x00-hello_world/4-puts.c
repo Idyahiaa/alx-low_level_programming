97449ea148ec80b1f0aa6477fe13d409604922b0
Mohamed marouane <mohmd8mar@gmail.com>
2023-06-19 14:31:22 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+/**
+ * main - puts
+ *
+ * Return: Always 0 (Success)
+ */
 
-int main(void) {
+int main(void){
 puts("Programming is like building a multilingual puzzle");
-return 0;
+return (0);
 }
