040643803d1c416cdb1474679072871ef70bbda7
Mohamed marouane <mohmd8mar@gmail.com>
2023-06-21 18:40:37 -0700

    m

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -5,14 +5,14 @@
  * @a: the fisrt int
  * @b: the seconde int
  *
- * Return : the result of teh sum of a and b
+ * Return: the result of the sum of a and b
  *
  */
 int add(int a, int b)
 {
 	int result;
 
-	result = a + b ;
+	result = a + b;
 
 	return (result);
 }
