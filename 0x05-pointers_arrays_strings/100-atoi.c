8a7e61e7547c6b0048ca3fcfdd3d2137b6b1fad7
Mohamed marouane <mohmd8mar@gmail.com>
2023-06-27 18:23:03 -0700

    9

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *_atoi - convert a string to an integer
+ * @s: the string to be converted 
+ * Return: integer resulty of conversion
+ */
+int _atoi(char *s)
+{
+	unsigned int n = 0; 
+	int sign = 1;
+
+	do {
+		if (*s == '-')
+			sign *= -1;
+		else if (*s >= '0' && *s <= '9')
+			n = (n * 10) + (*s - '0');
+		else if (n > 0)
+			break;
+	} while (*s++);
+
+	return (n * sign);
+}
